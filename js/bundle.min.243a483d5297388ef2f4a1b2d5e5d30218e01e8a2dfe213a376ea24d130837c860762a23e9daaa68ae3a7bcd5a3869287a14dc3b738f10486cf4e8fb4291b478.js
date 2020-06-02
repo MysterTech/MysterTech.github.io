c48d43b34e2b3932bd249d7721e7c29b0308c717
C M Sanjay Krishna <cmsanjaykrishna@gmail.com>
2020-06-02 18:16:35 +0530

    added scroll up and scroll down effect to top bar

--- js/bundle.min.243a483d5297388ef2f4a1b2d5e5d30218e01e8a2dfe213a376ea24d130837c860762a23e9daaa68ae3a7bcd5a3869287a14dc3b738f10486cf4e8fb4291b478.js
+++ js/bundle.min.243a483d5297388ef2f4a1b2d5e5d30218e01e8a2dfe213a376ea24d130837c860762a23e9daaa68ae3a7bcd5a3869287a14dc3b738f10486cf4e8fb4291b478.js
@@ -6,3 +6,16 @@ window.addEventListener("load", paddingHeader);
 function cleanForm() {
   document.getElementById("contact-form").reset();
 }
+
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-75px";
+  }
+
+  prevScrollPos = currentScrollPos;
+};
